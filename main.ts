785dfeb3b09171fa7d47c5cef8c1bc66a6009ab7
smeza0317 <shellymxza@outlook.com>
2024-09-23 20:19:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let Customer = sprites.create(img`
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f e e e 4 4 4 4 e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . f e e e e e e e e e e e e f . 
+    . f e e f f e e e e f f e e f . 
+    . f e e f b f e e f b f e e f . 
+    . f e e e 1 f 4 4 f 1 e e e f . 
+    f f e e f 4 4 4 4 4 4 f e e f f 
+    e e e f f f e e e e f f f e e f 
+    . f e e f 4 d 4 4 d 4 f e e f . 
+    . . e 4 4 d d d d d d 4 4 e . . 
+    . . e f 4 4 4 4 4 4 4 4 f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f d d f f . . . . . 
+    `, SpriteKind.Player)
